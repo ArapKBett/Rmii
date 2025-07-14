018d9153cb7f04d2cb88ef1eaad2bff56d6e3618
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-14 15:06:37 +0300

    Update MathClient.java

--- src/MathClient.java
+++ src/MathClient.java
@@ -5,7 +5,7 @@ public class MathClient {
     public static void main(String[] args) {
         try {
             // Replace "localhost" with Render's service URL or IP
-            Registry registry = LocateRegistry.getRegistry("your-render-service.onrender.com", 1099);
+            Registry registry = LocateRegistry.getRegistry("https://rmii.onrender.com", 1099);
             IRemoteMath remoteMath = (IRemoteMath) registry.lookup("Compute");
             System.out.println("1.7 + 2.8 = " + remoteMath.add(1.7, 2.8));
             System.out.println("6.7 - 2.3 = " + remoteMath.subtract(6.7, 2.3));
